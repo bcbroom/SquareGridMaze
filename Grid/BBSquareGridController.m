1d9928520d55dd644dafe06767e4b2ab26357bf2
Brian Broom <brian.broom@gmail.com>
2015-03-02 11:04:23 -0500

    Made totalHeight and totalWidth dynamic properties, calculations now based on them. Add method rectForFace:

--- Grid/BBSquareGridController.m
+++ Grid/BBSquareGridController.m
@@ -20,6 +20,9 @@
 
 @property (strong, nonatomic) BBSquareGrid *grid;
 
+@property (assign, nonatomic, readonly) NSInteger totalHeight;
+@property (assign, nonatomic, readonly) NSInteger totalWidth;
+
 @end
 
 @implementation BBSquareGridController
@@ -36,9 +39,26 @@
     return self;
 }
 
+- (NSInteger)totalHeight {
+    return self.grid.height * self.faceHeight + 2 * self.padding;
+}
+
+- (NSInteger)totalWidth {
+    return self.grid.width * self.faceWidth + 2 * self.padding;
+}
+
 - (CGPoint)pointForFaceCenter:(BBFace *)face {
     return CGPointMake(face.column * self.faceWidth + self.faceWidth/2 + self.padding,
                 face.row * self.faceHeight + self.faceHeight/2 + self.padding);
+}
+
+- (CGRect)rectForFace:(BBFace *)face {
+    return CGRectMake(face.column * self.faceWidth + self.padding,
+                      self.totalHeight - (face.row * self.faceHeight + self.padding + self.faceHeight), self.faceWidth, self.faceHeight);
+}
+
+- (CGPoint)pointForVertex:(BBVertex *)vertex {
+    return CGPointMake(0, 0);
 }
 
 - (CGPoint)centerToWallDistance {
@@ -46,14 +66,11 @@
 }
 
 - (UIImage *)renderGrid {
-    NSInteger totalHeight = self.grid.height * self.faceHeight + 2 * self.padding;
-    NSInteger totalWidth = self.grid.width * self.faceWidth + 2 * self.padding;
-    
-    UIGraphicsBeginImageContextWithOptions(CGSizeMake(totalWidth, totalHeight), YES, 0);
+    UIGraphicsBeginImageContextWithOptions(CGSizeMake(self.totalWidth, self.totalHeight), YES, 0);
     CGContextRef ctx = UIGraphicsGetCurrentContext();
     
     [[UIColor lightGrayColor] setFill];
-    CGContextFillRect(ctx, CGRectMake(0, 0, totalWidth, totalHeight));
+    CGContextFillRect(ctx, CGRectMake(0, 0, self.totalWidth, self.totalHeight));
     
     // init for random colors
     srand48(time(0));
@@ -62,8 +79,7 @@
     
     for (BBFace *face in [self.grid allFaces]) {
         [[UIColor colorWithHue:drand48() saturation:0.2 brightness:0.8 alpha:1.0] setFill];
-        CGContextFillRect(ctx, CGRectMake(face.column * self.faceWidth + self.padding,
-                                          totalHeight - (face.row * self.faceHeight + self.padding + self.faceHeight), self.faceWidth, self.faceHeight));
+        CGContextFillRect(ctx, [self rectForFace:face]);
         
 //        [[UIColor blackColor] setFill];
 //        
@@ -83,18 +99,20 @@
             CGContextSetLineWidth(ctx, 1.0);
         }
         
+        NSArray *verts = [self.grid endpointsForEdge:edge];
+        
         if ([edge.side isEqualToString:@"S"]) {
             CGContextMoveToPoint(ctx, edge.column * self.faceWidth + self.padding,
-                                 totalHeight - (edge.row * self.faceHeight + self.padding));
+                                 self.totalHeight - (edge.row * self.faceHeight + self.padding));
             CGContextAddLineToPoint(ctx, (edge.column + 1) * self.faceWidth + self.padding,
-                                    totalHeight - (edge.row * self.faceHeight + self.padding));
+                                    self.totalHeight - (edge.row * self.faceHeight + self.padding));
         }
         
         if ([edge.side isEqualToString:@"W"]) {
             CGContextMoveToPoint(ctx, edge.column * self.faceWidth  + self.padding,
-                                 totalHeight - (edge.row * self.faceHeight + self.padding));
+                                 self.totalHeight - (edge.row * self.faceHeight + self.padding));
             CGContextAddLineToPoint(ctx, edge.column * self.faceWidth + self.padding,
-                                    totalHeight - ((edge.row + 1) * self.faceHeight + self.padding));
+                                    self.totalHeight - ((edge.row + 1) * self.faceHeight + self.padding));
         }
         CGContextStrokePath(ctx);
     }
